2789d753a74bb9bf77c4a016a50f9eaad1eb7642
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-01-14 13:26:23 +0900

    Initial commit

---
+++ Assets/Forcer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Forcer : MonoBehaviour
+{
+    public Vector3 m_forceDirection = Vector3.up;
+    public float m_forcePower = 250.0f;
+    public ForceMode m_forceMode = ForceMode.Impulse;
+    Vector3 m_initialPosition = Vector3.zero;
+
+    void Start()
+    {
+        m_initialPosition = transform.position;
+    }
+
+    public void Attack()
+    {
+        GetComponent<Rigidbody>().AddForce(m_forcePower * m_forceDirection, m_forceMode);
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = m_initialPosition;
+    }
+}
